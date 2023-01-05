8ab35e4ce5edeb8673302f851a7ba22e101104e8
John Nguyen <jtnguyen236@gmail.com>
2023-01-05 22:32:59 +0800

    add button to jump to top

--- src/App.tsx
+++ src/App.tsx
@@ -559,7 +559,36 @@ const TerminalController = (props: {
       >
         {props.text}
       </Terminal>
+      <TopButton />
     </div>
+  )
+}
+
+const TopButton = () => {
+  return (
+    <Flex
+      sx={{
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: ["block", "block", "none"],
+          alignItems: "center",
+          borderStyle: "solid",
+          borderRadius: "0.5rem",
+          borderWidth: "1px",
+          borderColor: "primary",
+          margin: "1rem",
+          padding: "1rem",
+          textAlign: "center",
+        }}
+      >
+        <Link variant="term" href="#">
+          Back to top
+        </Link>
+      </Box>
+    </Flex>
   )
 }
 
